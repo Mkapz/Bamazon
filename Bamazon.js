d6a24cb6491053e759327c09baf884a7bc1fcfda
unknown <mkap25@yahoo.com>
2018-01-25 02:15:51 -0500

    added another function

--- Bamazon.js
+++ Bamazon.js
@@ -1,11 +1,11 @@
-var inquirer = require('inquirer');
-var mysql = require('mysql');
+var inquirer= require('inquirer');
+var mysql= require('mysql');
 
-var amountOwed;
-var currentDepartment;
-var updateSales;
+var PricePay;
+var currentDepartment
+var SalesX;
 
-var connection = mysql.createConnection({
+var connection= mysql.createConnection({
 	host: 'localhost',
 	port: 3306,
 	user: 'root',
@@ -13,115 +13,71 @@ var connection = mysql.createConnection({
 	database: 'Bamazon_db'
 });
 
-//Establish Connection
 connection.connect(function(err){
-	if (err) throw err;
-	console.log('connected as id: ' + connection.threadId)
+	if(err) throw err;
+	console.log("connected as id:" + connection.threadID)	
 });
 
-//FUNCTIONS
-//=============================================================================
-
-//Displays all items available in store and then calls the place order function
-function showProducts(){
+function PromptProduct(){
 	connection.query('SELECT * FROM products', function(err, res){
-		if (err) throw err;
-		console.log('=================================================');
-		console.log('=================Items in Store==================');
-		console.log('=================================================');
+		if(err) throw err;
+		console.log("-----------------");
+		console.log("-----------------");
+		console.log("Current items in store");
+		console.log("------------------");
+		console.log("------------------");
 
-		for(i=0;i<res.length;i++){
-			console.log('Item ID:' + res[i].id + ' Product Name: ' + res[i].ProductName + ' Price: ' + '$' + res[i].Price + '(Quantity left: ' + res[i].StockQuantity + ')')
+		for(i=0; i<res.length; i++){
+			console.log('Item ID:' + res[i].id + 'Product Name: ' + res[i].ProductName + 'Price: ' + '$' + res[i].Price + 'Avaiable' + res[i].StockQuantity)
 		}
-		console.log('=================================================');
-		placeOrder();
-		})
+		order();		
+	})
 }
 
-//Prompts the user to place an order, fulfills the order, and then calls the new order function
-function placeOrder(){
+function order(){
 	inquirer.prompt([{
-		name: 'selectId',
-		message: 'Please enter the ID of the product you wish to purchase',
-		validate: function(value){
-			var valid = value.match(/^[0-9]+$/)
-			if(valid){
-				return true
-			}
-				return 'Please enter a valid Product ID'
-		}
-	},{
-		name:'selectQuantity',
-		message: 'How many of this product would you like to order?',
-		validate: function(value){
-			var valid = value.match(/^[0-9]+$/)
-			if(valid){
-				return true
-			}
-				return 'Please enter a numerical value'
-		}
-	}]).then(function(answer){
-	connection.query('SELECT * FROM products WHERE id = ?', [answer.selectId], function(err, res){
-		if(answer.selectQuantity > res[0].StockQuantity){
-			console.log('Insufficient Quantity');
-			console.log('This order has been cancelled');
-			console.log('');
-			newOrder();
-		}
-		else{
-			amountOwed = res[0].Price * answer.selectQuantity;
-			currentDepartment = res[0].DepartmentName;
-			console.log('Thanks for your order');
-			console.log('You owe $' + amountOwed);
-			console.log('');
-			//update products table
-			connection.query('UPDATE products SET ? Where ?', [{
-				StockQuantity: res[0].StockQuantity - answer.selectQuantity
-			},{
-				id: answer.selectId
-			}], function(err, res){});
-			//update departments table
-			logSaleToDepartment();
-			newOrder();
-		}
-	})
+		type: 'input',
+		message: 'please input the ID of the product you want',
+		name: 'id'
+	},
+	{
+		type: 'input',
+		message: 'how many would you like to buy?',
+		name: "StockQuantity"
 
-}, function(err, res){})
-};
+	}
+	]).then(function (toBuy){
+		var quantity= toBuy.StockQuantity;
+		var ItemID= toBuy.id;
+		connection.query('SELECT * FROM products WHERE id=', [toBuy.selectID], function(err, res){
+			if(toBuy.StockQuantity > res[0].StockQuantity){
+				console.log("sorry we do not have the product avaiable");
+				console.log("Order canceled");
+				nextOrder();
+			}else{
+				PricePay= res[0].Price * answer.StockQuantity;
+				currentDepartment= res[0].DepartmentName * answer.DepartmentName;
+				console.log("thank you for ordering")
+				console.log("you owe $" + PricePay);
+				connection.query('UPDATE products SET ? WHERE ?' , [{
+					StockQuantity: res[0].StockQuantity - answer.StockQuantity
+				nextOrder();
+				};
 
-//Allows the user to place a new order or end the connection
-function newOrder(){
+
+function nextOrder(){
 	inquirer.prompt([{
 		type: 'confirm',
 		name: 'choice',
-		message: 'Would you like to place another order?'
-	}]).then(function(answer){
-		if(answer.choice){
-			placeOrder();
-		}
-		else{
-			console.log('Thank you for shopping at Bamazon!');
+		message: 'would you like to place another order?'
+	}]).then(function(toBuy){
+		if(toBuy.choice){
+			placeorder();
+		}else{
+			console.log("thank you for shopping on Bamazon")
 			connection.end();
 		}
 	})
 };
 
-
-//functions to push the sales to the executive table
-function logSaleToDepartment(){
-	connection.query('SELECT * FROM departments WHERE DepartmentName = ?', [currentDepartment], function(err, res){
-		updateSales = res[0].TotalSales + amountOwed;
-		updateDepartmentTable();
-	})
-};
-
-function updateDepartmentTable(){
-		connection.query('UPDATE departments SET ? WHERE ?', [{
-		TotalSales: updateSales
-	},{
-		DepartmentName: currentDepartment
-	}], function(err, res){});
-};
-//Call the original function (all other functions are called within this function)
-//======================================================================
-showProducts();
+		
